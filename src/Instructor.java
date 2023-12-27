db09fedcc475e3d898c2dfd790315494b14a2654
Ibita <ibisbra91@gmail.com>
2023-12-27 17:17:17 -0300

    Creation of project MyYogaClasses

---
+++ src/Instructor.java
@@ -0,0 +1,9 @@
+public class Instructor {
+    int id;
+    String name;
+    String style;
+
+    public void showName(){
+        System.out.println(name);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Instructor myInstructor = new Instructor();
+        myInstructor.name = "Ibis Brito";
+        myInstructor.showName();
+        }
+    }
