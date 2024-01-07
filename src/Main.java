423cbd6422d48e928e05f4c1a465657f2557cf81
Ibita <ibisbra91@gmail.com>
2024-01-07 17:55:47 -0300

    Variables vs. Objects. Wrapper classes: conversions from primitive types to objects (non-primitive types)

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
     public static void main(String[] args) {
 
 
-/*        Instructor myInstructor = new Instructor("Amaya Sol Luna", "Hatha Yoga");
+        Instructor myInstructor = new Instructor("Amaya Sol Luna", "Hatha Yoga");
         System.out.println(myInstructor.name);
-        System.out.println(myInstructor.style);*/
+        System.out.println(myInstructor.style);
 
         Disciple myDisciple = new Disciple("Monkey D. Luffy", "luffy@sombrero.de.paja.mugiwara.com");
         myDisciple.setWeight(54.6);                   //setea el valor del atributo private 'weight'
@@ -18,30 +18,29 @@ public class Main {
         myDisciple.setPhoneNumber("55123698");
         System.out.println(myDisciple.getPhoneNumber());
 
+        /** @see
+         * @see https://profile.es/blog/clases-wrapper-envoltorio-en-java/
+         * @see */
+        int i = 0;                                     //i --> variable de tipo de datos Primitivo o simple (int)
+        String word = "wonder";                        //word -> OBJETO de tipo String. String es un caso de OBJETO de Java "atípico".
+                                                       //STRING No es tipo de dato primitivo/simple de Java ni es clase wrapper, SÍ es un tipo de OBJETO de Java.
 
+                                                       // En Java, tenemos Wrapper Class o clase 'envoltorio' NO PRIMITIVA de Java.
 
+                                                       // ("envoltorio" porque estas clases 'envuelven' los tipos de datos PRIMITIVOS/simples para manipular esos OBJETOS.
+                                                       // OBJETOS que 'equivalen' a los tipos de datos primitivos/simples de Java)
+                                                       // y les proveen métodos para manipular estos valores)
+                                                       //son clases diseñadas para ser un complemento de los tipos primitivos.
 
-        //setter: setea(asigna o modifica) el valor de la variable, pero no devuelve nada, porque el set cambia y/o cambia-y-valida
-         //getter: obtiene/valida el valor del atributo.
-         // Con los Getters obtenemos los datos de las variables. Acción que devuelve un valor (tipo de retorno es el tipo de dato del retorno)
+                                                       // clases wrapper/envoltorio en Java son: Integer, Float, Boolean, Character, Double, Long, Short,Byte
+                                                       // tipos de datos primitivos/simples en Java son: int, float, boolean, char, double, long, short, byte
+                                                       //String es un caso atípico en este sentido.+
+                                                       // String NO ES una clase Wrapper pues NO TIENE UN TIPO DE DATOS PRIMITIVO asociado a STRING.
 
-                                          //uso el set (que modifica el valor del atributo con el valor pasado como parámetro al invocar el propio método set)
-
-        //para que lo muestre, uso el getter dentro de un mostrar/sout o lo asigno a una variable y la muestro luego
-        //si al Getter no le asigné valor de retorno distinto del tipo de dato de ese atributo en esa clase,
-        // mostrará el valor por default para el tipo de dato con el que se declaró el atributo al que se le generó el getter,
-        // ya que el getter tiene como base un atributo declarado (con su tipo de datos).
-
-        //En este caso, la variable height fue declarada como double.
-        //Le generé automáticamente sus métodos get y set, teniendo en cuenta que la única modificación a su get fue agregarle la cadena "Mts."
-        // Y le cambié el tipo de retorno, a String (para manejar ese retorno como cadena),
-        //sin modificar directamente el número del retorno, o sea, utilizando como número, el valor del atributo en su forma básica, que fue declarado como atributo double,
-        //de este modo:
-          //uso el get del atributo. Se muestra ese valor por default en double que es 0.0
-                                                         //uso el get (que obtiene el valor del atributo) y lo muestra con el sout
-                                                        //con los Setters asignamos o cambiamos su valor.  Acción que no devuelve nada (tipo de retorno void), sólo realiza la modificación del valor de un atributo.
-
-        //para mostrar un valor específico de height usaría el método set correspondiente a ese atributo
+/*                                                        sources: https://www.programiz.com/java-programming/wrapper
+                                                                   https://profile.es/blog/clases-wrapper-envoltorio-en-java/
+                                                                   https://www.javatpoint.com/wrapper-class-in-java
+                                                                   https://www.w3schools.com/java/java_wrapper_classes.asp*/
 
 
 
