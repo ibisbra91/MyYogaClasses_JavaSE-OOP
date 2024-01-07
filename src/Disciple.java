bc7a63ae895eca8d526aee2842dc39b18532442e
Ibita <ibisbra91@gmail.com>
2024-01-06 21:24:21 -0300

    Getters and Setters. Rules of Business within the class. Validations to maintain its CONSISTENCY. Examples.

--- src/Disciple.java
+++ src/Disciple.java
@@ -1,5 +1,96 @@
 public class Disciple {
 
+    //click encima del nombre del atributo en la clase y    -> alt + insert  genera setters y los getters.
+    //Los métodos setters y los getters, validan los parámetros/atributos de la clase  instanciar (a partir del objeto que se cree fuera de ella 😊)
+    //se crean en la parte superior de la clase por default
+
+
+    //las Reglas del Negocio se definen DENTRO de la clase a la que pertenecen, en los getter y los setter.
+    //las validaciones hacen posible mantener la CONSISTENCIA de las las Reglas del Negocio del programa.
+
+    //los setter y getter, son los métodos que se encargan de 'validar' los parámetros.
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }    //si el Negocio contemplara alguna validación, podría realizarla a través de una expresión regular que valide esa estructura determinada. Podría ser en el Setter o el Getter, depende del contexto que estoy desarrollando.
+
+    public void setEmail(String email) {  //podría validar el email con una expresión regular acá
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    //una Regla del negocio es: los números de teléfonos deben tener 8 dígitos.
+    //esta validación la integro DENTRO de la clase Disciple, DENTRO del método set correspondiente al atributo phoneNumber.
+    /**DESCRIPTION: This method validates that the phone number has a maximum of 8 digits. If the phone number is 8 digits, assign that new value which is the parameter to the phoneNumber variable.  */
+    public void setPhoneNumber(String phoneNumber) {
+        if(phoneNumber.length() > 8){
+            System.out.println("The phone number must be a maximum of 8 digits.");    //ej de una validación (VALIDO EN EL SET), dependerá de las Reglas del Negocio.
+        } else if(phoneNumber.length() == 8){
+            this.phoneNumber = phoneNumber;
+        }
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getHeight() {
+
+        return height + " Mts.";
+    }
+
+    public String getWeight() {
+
+        return weight + " Kg.";
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public String getBlood() {
+        return blood;
+    }
+
+    public void setBlood(String blood) {
+        this.blood = blood;
+    }
+
     int id;
     private String name;
     private String email;
@@ -14,12 +105,24 @@ public class Disciple {
         System.out.println("Construyendo el Objeto Disciple con Constructor *VACÍO o por default*: ");
     }
 
+    //el constructor por inicialización, se encarga de crear el objeto con los parámetros OBLIGATORIOS MÍNIMOS requeridos para ese objeto según la Lógica de Negocio del programa
     Disciple(String name, String email){
         System.out.println("Construyendo el Objeto Disciple con Constructor *por inicialización*: " + name + " con email: " + email);
         this.name = name;
         this.email = email;
-        this.weight = 54.2;
-        this.height = 1.2;
-        System.out.println(weight + "Kg.");
     }
+
+
+    //reglas del negocio, deben establecerse dentro de la clase que se instancia (a través del objeto para acceder a sus valores).
+    //ej: siempre que se muestre el peso del discípulo, aparezca en Kg.
+/*    public void setWeight(double weight){
+        this.weight = weight;
+    }
+
+    public String getWeight(){
+        return this.weight + " Kg.";
+    }*/
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,50 @@
-import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
 
 
-       // showMenu();
+/*        Instructor myInstructor = new Instructor("Amaya Sol Luna", "Hatha Yoga");
+        System.out.println(myInstructor.name);
+        System.out.println(myInstructor.style);*/
 
-        Disciple myDiscipleIris = new Disciple("Iris Diaz", "idiaz@gmail.com");
+        Disciple myDisciple = new Disciple("Monkey D. Luffy", "luffy@sombrero.de.paja.mugiwara.com");
+        myDisciple.setWeight(54.6);                   //setea el valor del atributo private 'weight'
+        System.out.println(myDisciple.getWeight());   //muestra ese nuevo valor de ese atributo (con la modificación definida en su get: "Kg.")
+
+ //       myDisciple.setPhoneNumber("123456789");
+/*      String a = myDisciple.getPhoneNumber();
+        System.out.println(a);                         //retona null porque no se le ha asignado ningún valor aún a ese atributo "phoneNumber"*/
+
+        myDisciple.setPhoneNumber("55123698");
+        System.out.println(myDisciple.getPhoneNumber());
+
+
+
+
+        //setter: setea(asigna o modifica) el valor de la variable, pero no devuelve nada, porque el set cambia y/o cambia-y-valida
+         //getter: obtiene/valida el valor del atributo.
+         // Con los Getters obtenemos los datos de las variables. Acción que devuelve un valor (tipo de retorno es el tipo de dato del retorno)
+
+                                          //uso el set (que modifica el valor del atributo con el valor pasado como parámetro al invocar el propio método set)
+
+        //para que lo muestre, uso el getter dentro de un mostrar/sout o lo asigno a una variable y la muestro luego
+        //si al Getter no le asigné valor de retorno distinto del tipo de dato de ese atributo en esa clase,
+        // mostrará el valor por default para el tipo de dato con el que se declaró el atributo al que se le generó el getter,
+        // ya que el getter tiene como base un atributo declarado (con su tipo de datos).
+
+        //En este caso, la variable height fue declarada como double.
+        //Le generé automáticamente sus métodos get y set, teniendo en cuenta que la única modificación a su get fue agregarle la cadena "Mts."
+        // Y le cambié el tipo de retorno, a String (para manejar ese retorno como cadena),
+        //sin modificar directamente el número del retorno, o sea, utilizando como número, el valor del atributo en su forma básica, que fue declarado como atributo double,
+        //de este modo:
+          //uso el get del atributo. Se muestra ese valor por default en double que es 0.0
+                                                         //uso el get (que obtiene el valor del atributo) y lo muestra con el sout
+                                                        //con los Setters asignamos o cambiamos su valor.  Acción que no devuelve nada (tipo de retorno void), sólo realiza la modificación del valor de un atributo.
+
+        //para mostrar un valor específico de height usaría el método set correspondiente a ese atributo
+
+
+
 
 
 
