d21084348150955c27168725ed71f16f98f1247a
Ibita <ibisbra91@gmail.com>
2024-01-12 18:40:59 -0300

    Helper or nested class classifications. How to access them when they are static. Example.

--- src/Instructor.java
+++ src/Instructor.java
@@ -1,23 +1,24 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Instructor {
 
     static int id = 0;        //autoincrement
-    String name;
-    String email;       //agregado, pues el programa va a permitir elegir si soy un Instructor o si soy un Discípulo, y esa identificación será a partir de usar el email de cada uno.
-    String style;
+    private String name;
+    private String email;       //agregado, pues el programa va a permitir elegir si soy un Instructor o si soy un Discípulo, y esa identificación será a partir de usar el email de cada uno.
+    private String style;
 
-//sobrecarga de métodos (de constructores en este caso)
 
-    // constructor por default
     Instructor(){
         System.out.println("Construyendo el Objeto Instructor con Constructor *VACÍO o por default*: ");
     }
 
     // constructor por inicialización
     Instructor(String name, String style){
-        id++;                                                                                               //cuando cree un objeto Instructor nuevo, incrementa el valor de la variable es 1
+        id++;
         System.out.println("Construyendo el Objeto Instructor con Constructor *por inicialización*: " + name);
-        this.name = name;                   //así, cuando cree la instancia fuera de esta clase usando este Constructor, inicializo los atributos del objeto con valores por default
-        this.style = style;                 //es decir, no se inicializará ese objeto en null; sino que contendrá estos 2 valores por default, siempre que sea un OBJETO construido invocando este Constructor
+        this.name = name;
+        this.style = style;
     }
 
 
@@ -32,6 +33,52 @@ public class Instructor {
         System.out.println("ID Instructor: " + id);         //como id es 'static' puedo acceder a su valor sin escribir 'Instructor.id' dentro de la misma clase
     }
 
+    ArrayList<AvailableYogaClass> aA = new ArrayList<>();           //colección de OBJETOS de tipo AvailableYogaClass
 
+    public void addClass(Date date, String time){                   //incrementa la lista de clases de yoga de ese Instructor
+        aA.add(new Instructor.AvailableYogaClass(date, time));
+    }
 
+    public ArrayList<AvailableYogaClass> getaA(){                   //devuelve la lista completa de clases de yoga
+        return aA;
+    }
+
+    //NEGOCIO: un Instructor puede definir ciertas fechas en las que él está disponible. Un Instructor puede tener muchas clases disponibles.
+    //Las clases Helper (clases anidadas y 'static' dentro de otra clase externa) suelen usarse cuando así lo requiere la Lógica de Negocio,
+    //o, cuando empiece detecte un atributo cuya estructura de datos empieza a tener muchos datos -> es  momento de analizarlo pues puede que en ese caso sea óptimo que sean declarados dentro de una clase anidada (y static o no) dentro de la clase inicial.'
+    //una vía para resolverlo es crear un clase independiente para agendar/modificar una clase de yoga con determinado Instructor -> una clase anidada (Helper class) haría:
+    public static class AvailableYogaClass{
+        private int id_AvailableClass;
+        private Date date;   //Available Class
+        private String time;
+
+        public AvailableYogaClass(Date date, String time) {  //como mínimo una clase deberá tener fecha y hora
+            this.date = date;
+            this.time = time;
+        }
+
+        public int getId_AvailableClass() {
+            return id_AvailableClass;
+        }
+
+        public void setId_AvailableClass(int id_AvailableClass) {
+            this.id_AvailableClass = id_AvailableClass;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,9 @@
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
-
-        //Comprendiendo memorias stack y memoria Heap. Su uso. Rendimiento. --> Variable vs. Objeto: Un vistazo a la memoria
-        int i = 0;
-        int b = 2;
-        i = b;
-        //b = 0
-
-        Disciple d1 = new Disciple("Roronoa Zoro", "espadachin@gmail.com");
+ /*     Disciple d1 = new Disciple("Roronoa Zoro", "espadachin@gmail.com");
         Disciple d2 = new Disciple("Nami", "navegante@gmail.com");
 
         System.out.println(d1);
@@ -30,52 +24,19 @@ public class Main {
 
 
         System.out.println("\n");
-
+*/
 
 
         Instructor myInstructor = new Instructor("Amaya Sol Luna", "Hatha Yoga");
-        System.out.println(myInstructor.name);
-        System.out.println(myInstructor.style);
+        myInstructor.addClass(new Date(),"6am");
+        myInstructor.addClass(new Date(),"7am");
+        myInstructor.addClass(new Date(),"8am");
 
-        Disciple myDisciple = new Disciple("Monkey D. Luffy", "luffy@sombrero.de.paja.mugiwara.com");
-        myDisciple.setWeight(54.6);                   //setea el valor del atributo private 'weight'
-        System.out.println(myDisciple.getWeight());   //muestra ese nuevo valor de ese atributo (con la modificación definida en su get: "Kg.")
+      //  System.out.println(myInstructor.getaA());
 
- //       myDisciple.setPhoneNumber("123456789");
-/*      String a = myDisciple.getPhoneNumber();
-        System.out.println(a);                         //retona null porque no se le ha asignado ningún valor aún a ese atributo "phoneNumber"*/
-
-        myDisciple.setPhoneNumber("55123698");
-        System.out.println(myDisciple.getPhoneNumber());
-
-        /** @see
-         * @see https://profile.es/blog/clases-wrapper-envoltorio-en-java/
-         * @see */
-        int ii = 0;                                     //i --> variable de tipo de datos Primitivo o simple (int)
-        String word = "wonder";                        //word -> OBJETO de tipo String. String es un caso de OBJETO de Java "atípico".
-                                                       //STRING No es tipo de dato primitivo/simple de Java ni es clase wrapper, SÍ es un tipo de OBJETO de Java.
-
-                                                       // En Java, tenemos Wrapper Class o clase 'envoltorio' NO PRIMITIVA de Java.
-
-                                                       // ("envoltorio" porque estas clases 'envuelven' los tipos de datos PRIMITIVOS/simples para manipular esos OBJETOS.
-                                                       // OBJETOS que 'equivalen' a los tipos de datos primitivos/simples de Java)
-                                                       // y les proveen métodos para manipular estos valores)
-                                                       //son clases diseñadas para ser un complemento de los tipos primitivos.
-
-                                                       // clases wrapper/envoltorio en Java son: Integer, Float, Boolean, Character, Double, Long, Short,Byte
-                                                       // tipos de datos primitivos/simples en Java son: int, float, boolean, char, double, long, short, byte
-                                                       //String es un caso atípico en este sentido.+
-                                                       // String NO ES una clase Wrapper pues NO TIENE UN TIPO DE DATOS PRIMITIVO asociado a STRING.
-
-/*                                                        sources: https://www.programiz.com/java-programming/wrapper
-                                                                   https://profile.es/blog/clases-wrapper-envoltorio-en-java/
-                                                                   https://www.javatpoint.com/wrapper-class-in-java
-                                                                   https://www.w3schools.com/java/java_wrapper_classes.asp*/
-
-
-
-
-
+        for (Instructor.AvailableYogaClass aAA: myInstructor.getaA()) {
+            System.out.println(aAA.getDate() + " " + aAA.getTime());
+        }
 
 
         }
