e6a4bec6c809aa42f85ff027abed75ab8610522e
Ibita <ibisbra91@gmail.com>
2024-01-25 12:07:46 -0300

    simulating authentication. Using ArrayList<> I bring data from a Data Source in UIMenu.java class. Scannet class. NextLine() method.

--- src/ui/UIMenu.java
+++ src/ui/UIMenu.java
@@ -1,11 +1,16 @@
 package ui;
 
+import model.Instructor;
+import model.Disciple;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UIMenu {
 
     public static final String[] MONTHS = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-
+    public static Instructor instructorLoggead;
+    public static Disciple discipleLoggead;
 
     public static void showMenu(){
         System.out.println("Welcome to Our Yoga Classes");
@@ -13,8 +18,8 @@ public class UIMenu {
 
         int response = 0;
         do {
-            System.out.println("1. model.Instructor");
-            System.out.println("2. model.Disciple");
+            System.out.println("1. Instructor");
+            System.out.println("2. Disciple");
             System.out.println("0. Namasté");
             System.out.println("\n\n");
 
@@ -23,11 +28,15 @@ public class UIMenu {
 
             switch (response){
                 case 1:
-                    System.out.println("model.Instructor");
+                    System.out.println("Instructor");
+                    response = 0;
+                    authUser(1);;
                     break;
                 case 2:
                     response = 0;
-                    showDiscipleMenu();
+                    System.out.println("Disciple");
+                    authUser(2);
+                    //showDiscipleMenu();
 
                     break;
                 case 0:
@@ -37,6 +46,55 @@ public class UIMenu {
                     System.out.println("Please select a correct answer");
             }
         }while (response != 0);
+    }
+
+    //acá manejo la authentication, simulando con uso de ArrayList<> que traigo datos de una Fuente de Datos
+    private static void authUser(int userType){  //userType indica el resultado final que deberia traer
+        //userType = 1 Instructor
+        //userType = 2 Disciple
+        //aquí quiero simular como si tuviera una lista de Instructores y otra de Discipuls
+        //simulación => como si fuera a mi capa de persistencia de datos y me trajera todos los usuarios
+        //que tengo registrados allí con queries, según el tipo de usuarios que sea:
+        // Instructor o Disciple O Nutricionist (Nutricionist lo hago luego)
+        ArrayList<Instructor> instructors = new ArrayList<>();
+        instructors.add(new Instructor("Edward Newgate","barbablanca@endava.com"));
+        instructors.add(new Instructor("Shanks","pelirrojo@arsys.com"));
+        instructors.add(new Instructor("Monkey D. Luffy","mugiwara@globant.com"));
+
+        ArrayList<Disciple> disciples = new ArrayList<>();
+        disciples.add(new Disciple("Kozuki Momonosuke","shogunWano@sonda.uy"));
+        disciples.add(new Disciple("Carrot","mink@SignUp.es"));
+        disciples.add(new Disciple("Tony Tony Chopper","renoMedicoWano@universalSolutionstech.uy"));
+
+        boolean emailCorrect = false;  //por default va a estar en falso, hasta que haga la verificación
+        do{                             //y cambie a true
+            System.out.println("Insert your email. Example: qws@sdf.com");
+            Scanner sc = new Scanner(System.in);  //para leer lo que el user teclee
+            String email = sc.nextLine();
+            if(userType == 1){                     //si es 1, irá y verificará en el ArrayList<Instructor>. Si tuviera una BD, estos datos los tendría allí y me los traería con queries
+                for (Instructor i: instructors) {  //tengo que recorrer todos los Instructors hasta encontrar el email que recibí, de modo que el email entrdao por el user haga match con alguno de los emails que ya tengo registrados/existen/persistencia de datos
+                    if(i.getEmail().equals(email)){
+                        emailCorrect = true;
+                        //obtener los datos del user loggeado
+                        instructorLoggead = i;
+                        //showDoctorMenu
+
+                    }
+                }                                     //
+            }
+            if (userType == 2){                    //si es 2, irá y verificará en el ArrayList<Disciple>
+                for (Disciple d: disciples){
+                    if (d.getEmail().equals(email)){
+                        emailCorrect = true;
+                        //obtener los datos del user loggeado
+                        discipleLoggead = d;
+                        //showDiscipleMenu
+                    }
+                }
+
+            }
+        }while (!emailCorrect);
+
     }
 
 
